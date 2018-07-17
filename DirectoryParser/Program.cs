363996e0f09c5053aec3bbf392720c69162a67c4
Christopher Matthews <christopherematthews@gmail.com>
2018-07-17 17:27:25 -0400

    Added basic recursive search and stack-based search

--- DirectoryParser/Program.cs
+++ DirectoryParser/Program.cs
@@ -8,8 +8,52 @@ namespace DirectoryParser
 {
     class Program
     {
+        static System.Collections.Specialized.StringCollection log = new System.Collections.Specialized.StringCollection();
+
+        static void RecursiveMain(string[] args)
+        {
+            // Start with drives if you have to search the entire computer.
+            string[] drives = System.Environment.GetLogicalDrives();
+
+            foreach (string dr in drives)
+            {
+                System.IO.DriveInfo di = new System.IO.DriveInfo(dr);
+
+                // Here we skip the drive if it is not ready to be read. This
+                // is not necessarily the appropriate action in all scenarios.
+                if (!di.IsReady)
+                {
+                    Console.WriteLine("The drive {0} could not be read", di.Name);
+                    continue;
+                }
+                System.IO.DirectoryInfo rootDir = di.RootDirectory;
+                RecursiveFileSearch.WalkDirectoryTree(rootDir);
+            }
+
+            // Write out all the files that could not be processed.
+            Console.WriteLine("Files with restricted access:");
+            foreach (string s in log)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static void StackBasedMain(string[] args)
+        {
+            // Specify the starting folder on the command line, or in 
+            // Visual Studio in the Project > Properties > Debug pane.
+            StackBasedFileSearch.TraverseTree(args[0]);
+        }
+
         static void Main(string[] args)
         {
+            // Kick off either recursive or stack-based directory navigation
+            RecursiveMain(new List<String>().ToArray());
+            StackBasedMain(new List<String>().ToArray());
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
         }
     }
 }
